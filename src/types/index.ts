81fd1d581555f7e36f4ce9682d82bf32c3266169
JonatanSalas <jonatan.salas@blackbox-vision.tech>
2021-07-18 18:42:23 -0300

    feat: improve typings in library

--- src/types/index.ts
+++ src/types/index.ts
@@ -14,7 +14,40 @@ export type UseGoogleAnalyticsHook = (id: string) => void;
 
 // Api
 
+export type SupportedEvents =
+  | "add_payment_info"
+  | "add_to_cart"
+  | "add_to_wishlist"
+  | "begin_checkout"
+  | "checkout_progress"
+  | "generate_lead"
+  | "login"
+  | "purchase"
+  | "refund"
+  | "remove_from_cart"
+  | "search"
+  | "select_content"
+  | "set_checkout_option"
+  | "share"
+  | "sign_up"
+  | "view_item"
+  | "view_item_list"
+  | "view_promotion"
+  | "view_search_results"
+  | string;
+
+type SupportedCategories = "engagement" | "ecommerce" | string;
+
+type SupportedLabels = "method" | "search_term" | "content_type" | string;
+
+type EventParams = {
+  non_interaction?: boolean;
+  event_category?: SupportedCategories;
+  event_label?: SupportedLabels;
+  value?: string;
+};
+
 export type GoogleAnalyticsApi = {
   pageview: (url: string, id: string) => void;
-  event: (name: string, params: Record<string, any>) => void;
+  event: (name: SupportedEvents, params: EventParams) => void;
 };
